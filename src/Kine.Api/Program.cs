870d772ce60067c1b963277a304b139c5f3cc90b
Kine Dev Team <team@kine.dev>
2026-07-01 19:33:35 +0200

    Add solution file and build configuration

--- src/Kine.Api/Program.cs
+++ src/Kine.Api/Program.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Kine.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<TenantContextMiddleware>();
 
